5f0b81e5da2c73aba2a26669db7a84cd98823d4e
java-nerd21 <91192869+java-nerd21@users.noreply.github.com>
2024-03-05 12:09:47 +0000

    Completed high and low game
    Added compare function and the play game functions

--- Beginner/Day 14/high_and_low.py
+++ Beginner/Day 14/high_and_low.py
@@ -13,9 +13,54 @@ def format(account):
     country = account["country"]
     return f"{name}, {description}, from {country}."
 
+def compare_accounts(account_a, account_b):
+    """Compare two accounts followers"""
+    if account_a["follower_count"] > account_b["follower_count"]:
+        return "A"
+    elif account_a["follower_count"] < account_b["follower_count"]:
+        return "B"
+    else:
+        return "C"
+
+
+
+def play_game(account_a, account_b, count):
+    print(logo)
+    
+    is_game_over = False
+    
+    while not is_game_over:
+        while account_a == account_b:
+            account_b = random_account()
+
+        print("Compare A: " + format(account_a))
+        print(vs)
+        print("Compare B: " + format(account_b))
+
+        """Returns which player has more followers"""
+        answer = compare_accounts(account_a, account_b)
+
+        user_answer = input("Who has more followers? Type 'A' or 'B': ")
+
+        if user_answer == answer:
+            count = count + 1
+            print(logo)
+            print("You are correct! Current score is " + str(count))
+        else:
+            print("You are incorrect! Final score is " + str(count))
+            is_game_over = True
+
+        account_a = account_b
+        
 
 account_a = random_account()
-account_b = random_account()
-print("Compare A: " + format(account_a))
+account_b = account_a
+count = 0
+
+play_game(account_a, account_b, count)
+
+
+
+
 
 
