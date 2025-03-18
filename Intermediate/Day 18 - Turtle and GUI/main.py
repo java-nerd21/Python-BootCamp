c8404b801d93778173aee7c5cdaef924439eb443
java-nerd21 <mightylogix@gmail.com>
2025-03-18 15:20:37 +0000

    #Day 18 Challenge 3 - Drawing Different Shapes

--- Intermediate/Day 18 - Turtle and GUI/main.py
+++ Intermediate/Day 18 - Turtle and GUI/main.py
@@ -10,15 +10,21 @@ timmy.color("red")
 #     timmy.right(90)
 
 #Challenge 2 draw a dashed line
-for _ in range(15):
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
-    timmy.pendown()
+# for _ in range(15):
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
+#     timmy.pendown()
 
+#Challenge 3 - Drawing Different Shapes
+colors = ["red", "blue", "green", "black", "purple", "orange"]
 
+for i in range(3, 11): #number of sides
+    timmy.color(colors[i-3]) #color of the shape
 
-
+    for _ in range(i):
+        timmy.forward(100) #length of each side
+        timmy.right(360/i) #angle = 360/number of sides
 
 
 
