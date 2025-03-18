caf30db5c05f892db0c6a6b8b2146484406990a0
java-nerd21 <mightylogix@gmail.com>
2025-03-18 15:00:33 +0000

    #Day 18 Challenge 1 drawing a square

---
+++ Intermediate/Day 18 - Turtle and GUI/main.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+
+#Challenge 1 draw a square
+for _ in range(4):
+    timmy.forward(100)
+    timmy.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Screen = Screen()
+Screen.exitonclick()
