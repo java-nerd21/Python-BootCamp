d0600fd50c6e90c820ff369602d649e9321cf598
java-nerd21 <mightylogix@gmail.com>
2025-03-18 15:11:06 +0000

    #Day 19 Challenge 2 draw a dashed line

--- Intermediate/Day 18 - Turtle and GUI/main.py
+++ Intermediate/Day 18 - Turtle and GUI/main.py
@@ -5,11 +5,16 @@ timmy.shape("turtle")
 timmy.color("red")
 
 #Challenge 1 draw a square
-for _ in range(4):
-    timmy.forward(100)
-    timmy.right(90)
+# for _ in range(4):
+#     timmy.forward(100)
+#     timmy.right(90)
 
-
+#Challenge 2 draw a dashed line
+for _ in range(15):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
 
 
 
