0e5b352e23dc4bca548e0e4cd4c2db08b635abbb
java-nerd21 <mightylogix@gmail.com>
2025-03-18 14:16:52 +0000

    Created QuizBrain Object with methods and code to run quiz

--- Intermediate/Day 17 - Start/main.py
+++ Intermediate/Day 17 - Start/main.py
@@ -1,10 +1,17 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
-for question in question_data:
+for question in question_data: # creates a list of Question objects
     question_text = question["text"]
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions(): # loops through the questions
+    quiz.next_question()
+else:
+    quiz.quiz_end()
