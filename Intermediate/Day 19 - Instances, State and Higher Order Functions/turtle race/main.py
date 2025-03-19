312d67304590f9f8ab8f7bc9694b5e0dcd161d54
java-nerd21 <mightylogix@gmail.com>
2025-03-19 11:43:37 +0000

    #Day 19 created turtle race

--- Intermediate/Day 19 - Instances, State and Higher Order Functions/turtle race/main.py
+++ Intermediate/Day 19 - Instances, State and Higher Order Functions/turtle race/main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 import random
 
+
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 
@@ -16,5 +18,22 @@ for i in range(0, 6):
     new_turtle.goto(x=-230, y=y_positions[i])
     all_turtles.append(new_turtle)
 
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+   # random_distance = random.randint(0, 10)
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You lost! The {winning_color} turtle is the winner!")
+
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 screen.exitonclick()
